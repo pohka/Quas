5c35790a84ff06416d6ef48f5236703c2a95bbdb
geff <zomg21@hotmail.com>
2018-01-05 02:30:55 +0000

    removed old functions Bwe is not object oriented

--- bwe/bwe.js
+++ bwe/bwe.js
@@ -1,212 +1,226 @@
-class bwe{
-  //builds a html string from the json
-  static build(tags){
-    if(tags.constructor !== Array){
-      var tags = [tags];
-    }
-    let res = "";
-    for(let tag in tags){
-      let kv = tags[tag];
-      res += "<" + kv["tag"];
-
-      for(var i in bwe.identifiers){
-        var key = bwe.identifiers[i];
-        if(kv[key] != undefined && key!=="data"){
-          res += " " + key + "='" + kv[key] + "'";
-        }
-      }
-
-      for(var i in bwe.idendifiersNoVal){
-        var key = bwe.idendifiersNoVal[i];
-        if(kv[key] != undefined){
-          res += " " + key;
-        }
-      }
-
-      if(kv["data"] != undefined){
-        for(var key in kv["data"]){
-            res += " data-" + key + "='" + kv["data"][key] + "'";
-        }
-      }
-
-      var requiresClosingTag = $.inArray(kv["tag"], bwe.noClosingTag);
-
-      if(requiresClosingTag != -1){
-        res += ">";
-      }
-      else{
-        res += ">";
-      }
-
-      if(kv["con"] != undefined){
-        res += kv["con"];
-      }
-      for(var i in kv["children"]){
-        res+= bwe.build(kv["children"][i]);
-      }
-
-      if(requiresClosingTag == -1){
-        res += "</" + kv["tag"] + ">\n";
-      }
-    }
-    return res;
+class Bwe{
+  constructor(sel, data){
+    this.sel = sel;
+    this.data = data;
   }
 
-  //builds a html string from the json and appends it to the selector's html
-  static append(sel, kv){
-    var res = bwe.build(kv);
-    $(sel).append(res);
+  render(){
+    let res = Bwe.build(this.data);
+    $(this.sel).append(res);
   }
+}
 
-  //generates json for a list
-  static genList(kv, items){
-    if(kv["children"] == undefined){
-      kv["children"] = [];
-    }
-    if(kv["tag"] == undefined){
-      kv["tag"] = "ul";
-    }
-
-    for(var i in items){
-      kv["children"].push({
-        tag : "li",
-        con : items[i]
-      });
-    }
-    return kv;
+//builds a html string from the json
+Bwe.build = function(tags){
+  if(tags.constructor !== Array){
+    var tags = [tags];
   }
+  let res = "";
+  for(let tag in tags){
+    let kv = tags[tag];
+    res += "<" + kv["tag"];
 
-  //generates json for a table
-  static genTable(kv, rows){
-    if(kv["children"] == undefined){
-      kv["children"] = [];
-    }
-    kv["tag"] = "table";
-
-    //headings row
-    var headings = {
-      tag : "tr",
-      children : []
-    };
-    for(var c=0; c < rows[0].length; c++){
-      headings["children"].push({
-        tag : "th",
-        con : rows[0][c]
-      });
-    }
-    kv["children"].push(headings);
-
-    //data rows
-    for(var r=1; r < rows.length; r++){
-      var row = {
-        tag : "tr",
-        children : []
-      };
-      for(var c in rows[r]){
-        row["children"].push({
-          tag : "td",
-          con : rows[r][c]
-        });
-      }
-
-      kv["children"].push(row);
-    }
-    return kv;
-  }
-
-  //generate json for navbar items
-  static genNavItems(itemAttrs, conAttrs){
-    var curPage = bwe.getCurPage();
-    var navChildren = [];
-    for(var i in bwe.pages){
-      var cls = "";
-      var pageName = bwe.pages[i]["name"];
-      if(curPage == pageName )
-        cls = "active";
-
-      var item = {
-        tag : "a",
-        con : bwe.pages[i]["name"],
-        class : cls,
-        href  : bwe.pages[i]["page"]
-      };
-
-      //attributes of each item
-      if(itemAttrs != undefined){
-        for(var key in itemAttrs){
-          item = bwe.appendAttr(item, key, itemAttrs[key]);
-        }
-
-        //attributes of content
-        if(conAttrs != undefined){
-
-          var con = {};
-          for(var key in conAttrs){
-            con[key] = conAttrs[key];
-          }
-          con["con"] = bwe.pages[i]["name"];
-          item["con"] = "";
-          //item["children"].push(con);
-          item = bwe.appendAttr(item, "children", con);
-        }
-      }
-
-      navChildren.push(item);
-    }
-    return navChildren;
-  }
-
-  //returns the name of the current page in bwe.pages
-  static getCurPage(){
-    var curPage = location.pathname;
-
-    //if not being run on a virtual host
-    if(window.location.hostname == ""){
-      curPage = curPage.split("/").pop()
-    }
-    for(var i in bwe.pages){
-      var page = bwe.pages[i]["page"];
-      if(
-          curPage == page ||
-          curPage == "/" + page ||
-          curPage == page.replace(".html", "") ||
-          (curPage == "/" && (page == "/index" || page == "/index.html" || page == "index" || page == "index.html"))
-        ){
-        return bwe.pages[i]["name"];
+    for(var i in Bwe.identifiers){
+      var key = Bwe.identifiers[i];
+      if(kv[key] != undefined && key!=="data"){
+        res += " " + key + "='" + kv[key] + "'";
       }
     }
-    console.log("Current page not found");
-  }
 
-  //appends the value an attribute of an item,
-  //if it doesn't exist then it adds the attribute to the item
-  //returns json for the item
-  static appendAttr(item, key, val){
-    if(!item.hasOwnProperty(key)){
-      if(key != "data" && key != "children"){
-        item[key] = "";
-      }
-      else {
-        item[key] = [];
+    for(var i in Bwe.idendifiersNoVal){
+      var key = Bwe.idendifiersNoVal[i];
+      if(kv[key] != undefined){
+        res += " " + key;
       }
     }
-    if(key == "data" || key == "children"){
-      item[key].push(val);
+
+    if(kv["data"] != undefined){
+      for(var key in kv["data"]){
+          res += " data-" + key + "='" + kv["data"][key] + "'";
+      }
     }
-    else if(key == "class"){
-      item[key] += " " + val;
+
+    var requiresClosingTag = $.inArray(kv["tag"], Bwe.noClosingTag);
+
+    if(requiresClosingTag != -1){
+      res += ">";
     }
     else{
-      item[key] += val;
+      res += ">";
     }
-    return item;
+
+    if(kv["con"] != undefined){
+      res += kv["con"];
+    }
+    for(var i in kv["children"]){
+      res+= Bwe.build(kv["children"][i]);
+    }
+
+    if(requiresClosingTag == -1){
+      res += "</" + kv["tag"] + ">\n";
+    }
   }
+  return res;
+}
+
+
+//converts a html string to json
+Bwe.convertToJSON = function(html){
+  var start = 0;
+  var openQuote = false;
+  var openSingleQuote = false;
+  var openDoubleQuote = false;
+  var isClosing = false;
+  let readContent = false;
+  var attr = ""; //characters of the current atttribute
+  var ch; //the current character
+  var attrs = []; //all the parsed attributes
+  var tags = []; //a list json objects for tags
+  var activeParentIndex = -1;
+  var childDepth = -1;
+  var childIndexes = [];
+  let content = "";
+  for(var i=0; i<html.length; i++){
+    var ch = html.charAt(i);
+    if(ch === "<"){
+      start = i;
+      if(html.charAt(i+1) === "/"){
+        isClosing = true;
+        tags[tags.length-1]["con"] = content
+        content = "";
+        readContent=false;
+      }
+    }
+    else if(ch === ">"){
+      if(attr !== ""){
+        attrs.push(attr);
+        attr = "";
+      }
+      var json = Bwe.genFromAttrs(attrs);
+      attrs = [];
+      //opening tag
+      if(!isClosing){
+        readContent = true;
+        childDepth += 1;
+        //root tag
+        if(childDepth == 0){
+          tags.push(json);
+          activeParentIndex += 1;
+        }
+        //child tag
+        else{
+          //add childIndex if it doesnt exist
+          if(childIndexes.length <= childDepth){
+            childIndexes.push(0);
+          }
+          else{
+            childIndexes[childDepth] += 1;
+          }
+
+          var parent = tags[activeParentIndex];
+          for(var c = 0; c < childDepth-1; c++){
+            var index = childIndexes[c];
+            if(!("children" in parent)){
+              parent["children"] = [];
+            }
+            parent = parent["children"][index];
+          }
+          if(!parent.hasOwnProperty("children")){
+            parent["children"] = [];
+          }
+         parent["children"].push(json);
+
+         if($.inArray(json["tag"], Bwe.noClosingTag) == false){
+           childIndexes[childDepth] = -1;
+           childDepth -= 1;
+           childIndexes[childDepth] += 1;
+           isClosing=false;
+         }
+        }
+      }
+      //closing tag
+      else{
+        childIndexes[childDepth] = -1;
+        childDepth -= 1;
+        childIndexes[childDepth] += 1;
+        isClosing=false;
+      }
+    }
+    else if(readContent){
+      content += ch;
+    }
+    else if(ch == "\'"){
+      openSingleQuote = !openSingleQuote
+      if(openDoubleQuote == false && openSingleQuote == true){
+        openQuote = true;
+      }
+      else if(openSingleQuote == false){
+        openQuote = false;
+      }
+    }
+    else if(ch == "\""){
+      openDoubleQuote = !openDoubleQuote;
+      if(openDoubleQuote == true && openSingleQuote == false){
+        openQuote = true;
+      }
+      else if(openDoubleQuote == false){
+        openQuote = false;
+      }
+    }
+    else if(!isClosing){
+      if(openQuote == false && ((ch == ' ') || (ch == '\t') || (ch == '\n'))){
+        if(attr !== ""){
+          attrs.push(attr);
+          attr = "";
+        }
+      }
+      else{
+        attr += ch;
+      }
+    }
+  }
+  let str = JSON.stringify(tags);
+  str = str.substr(1,str.length-2);
+  $("#builder-json").val(str);
+  $("#builder-result").val(Bwe.build(tags));
+}
+
+
+Bwe.genFromAttrs = function(attrs){
+  var json = {
+    tag : attrs[0]
+  };
+  for(var i=1; i<attrs.length; i++){
+    if(attrs[i].indexOf("=") > -1){
+      var els = attrs[i].split("=");
+      if(els[0].indexOf("data-") > -1){
+        let key = els[0].replace("data-", "");
+        if(json["data"] === undefined){
+          json["data"] = {};
+        }
+        json["data"][key] = els[1];
+      }
+      else{
+        json[els[0]] = els[1].replace(/"/g , "");
+      }
+    }
+    else if(Bwe.idendifiersNoVal.indexOf(attrs[i]) > -1){
+      json[attrs[i]] = "";
+    }
+    else{
+      if(!json.hasOwnProperty("con")){
+        json["con"] = "";
+      }
+      json["con"] += attrs[i];
+    }
+  }
+  return json;
 }
 
 
 
 //tag elements with a value
-bwe.identifiers = [
+Bwe.identifiers = [
   "accept", "accept-charset", "accesskey", "action", "alt", "autocomplete", "charset", "cite", "class",
   "cols", "colspan", "content", "contenteditable", "coords", "data", "dir", "dirname", "draggable",
   "enctype", "for", "form", "formaction", "headers", "height", "href", "hreflang", "http-equiv",
@@ -224,7 +238,7 @@ bwe.identifiers = [
 ];
 
 //tag elements with no value
-bwe.idendifiersNoVal = [
+Bwe.idendifiersNoVal = [
   "async", "autofocus", "autoplay", "checked", "controls",
   "default", "defer", "disabled", "download", "hidden", "high",
   "ismap", "loop", "multiple", "muted", "novalidate",
@@ -232,8 +246,8 @@ bwe.idendifiersNoVal = [
 ];
 
 //tags that require no closing tag
-bwe.noClosingTag = ["img", "source", "br", "hr", "area", "track", "link", "col", "meta", "base", "embed", "param", "input"];
+Bwe.noClosingTag = ["img", "source", "br", "hr", "area", "track", "link", "col", "meta", "base", "embed", "param", "input"];
 
 
 //pages that are used in the navbar
-bwe.pages = [];
+Bwe.pages = [];
