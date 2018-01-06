ecf47fb32bef96b77fc229ca3c0691aeb90858d1
geff <zomg21@hotmail.com>
2018-01-06 07:34:40 +0000

    moved json to html string build to new file and new minified bwe.js

--- bwe/bwe.js
+++ bwe/bwe.js
@@ -251,60 +251,6 @@ class Bwe{
     }
   }
 
-  //builds a html string from the json
-  static build(tags){
-    if(tags.constructor !== Array){
-      var tags = [tags];
-    }
-    let res = "";
-    for(let tag in tags){
-      let kv = tags[tag];
-      res += "<" + kv["tag"];
-
-      for(var i in Bwe.identifiers){
-        var key = Bwe.identifiers[i];
-        if(kv[key] != undefined && key!=="data"){
-          res += " " + key + "='" + kv[key] + "'";
-        }
-      }
-
-      for(var i in Bwe.idendifiersNoVal){
-        var key = Bwe.idendifiersNoVal[i];
-        if(kv[key] != undefined){
-          res += " " + key;
-        }
-      }
-
-      if(kv["data"] != undefined){
-        for(var key in kv["data"]){
-            res += " data-" + key + "='" + kv["data"][key] + "'";
-        }
-      }
-
-      var requiresClosingTag = $.inArray(kv["tag"], Bwe.noClosingTag);
-
-      if(requiresClosingTag != -1){
-        res += ">";
-      }
-      else{
-        res += ">";
-      }
-
-      if(kv["txt"] != undefined){
-        res += kv["txt"];
-      }
-      for(var i in kv["children"]){
-        res+= Bwe.build(kv["children"][i]);
-      }
-
-      if(requiresClosingTag == -1){
-        res += "</" + kv["tag"] + ">\n";
-      }
-    }
-    return res;
-  }
-
-
   //converts a html string to json
   static makeData(html){
     var start = 0;
@@ -565,41 +511,3 @@ class Bwe{
 }
 Bwe.isScrollable = true;
 Bwe.scrollKeys = {37: 1, 38: 1, 39: 1, 40: 1};
-
-//tag elements with a value
-Bwe.identifiers = [
-  "accept", "accept-charset", "accesskey", "action", "alt", "autocomplete", "charset", "cite", "class",
-  "cols", "colspan", "content", "contenteditable", "coords", "data", "dir", "dirname", "draggable",
-  "enctype", "for", "form", "formaction", "headers", "height", "href", "hreflang", "http-equiv",
-  "id", "kind", "label", "lang", "list", "low", "max", "maxlength", "media", "method", "min", "name",
-  "onseeked", "onseeking", "onselect", "onsubmit", "ontimeupdate", "onunload", "onvolumechange", "onwaiting",
-  "optimum", "pattern", "placeholder", "poster", "preload", "rel", "rows", "rowspan", "shape", "size",
-  "span", "spellcheck", "src", "srcdoc", "srclang", "srcset", "start", "step", "style", "tabindex",
-  "target", "title", "type", "usemap", "value", "width", "wrap"
-];
-
-
-//tag elements with no value
-Bwe.idendifiersNoVal = [
-  "async", "autofocus", "autoplay", "checked", "controls",
-  "default", "defer", "disabled", "download", "hidden", "high",
-  "ismap", "loop", "multiple", "muted", "novalidate",
-  "readonly", "required", "reversed", "sandbox"
-];
-
-//event listeners
-Bwe.events = [
-  "beforeunload", "blur", "canplay", "canplaythrough", "change", "click",
-  "ctextmenu", "copy", "cut", "dblclick", "drag", "dragend", "dragenter", "dragleave",
-  "dragover", "dragstart", "drop", "duratichange", "ended", "error", "focus", "hashchange",
-  "input", "invalid", "keydown", "keypress", "keyup", "load", "loadeddata", "loadedmetadata",
-  "loadstart", "mousedown", "mousemove", "mouseout", "mouseover", "mouseup", "pageshow",
-  "paste", "pause", "play", "playing", "progress", "ratechange", "reset", "resize", "scroll"
-];
-
-//tags that require no closing tag
-Bwe.noClosingTag = ["img", "source", "br", "hr", "area", "track", "link", "col", "meta", "base", "embed", "param", "input"];
-
-
-//pages that are used in the navbar
-Bwe.pages = [];
